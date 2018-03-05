d3dc65b122be765f0b25af76961392adc4a9bd05
matmorel <pub.imagiin@gmail.com>
2018-03-05 10:58:16 +0100

    initial commit

---
+++ src/components/create.vue
@@ -0,0 +1,59 @@
+<template>
+<form @submit.prevent="onSubmit()">
+  <div class="form-group">
+    <input type="text" name="title" placeholder="Title" v-model="title">    
+  </div>
+  <div
+  v-for="(option, index) in options" 
+  :key="index"  class="form-group">  
+    <input type="text" 
+    :name="`option${index + 1}`"
+    :placeholder="`Option ${index + 1}`"
+    v-model="options[index]">
+
+    <button
+    type="button"
+    class="btn btn-link"
+    @click="remove(index)"
+    v-if="options.length > 2">
+      Remove
+    </button>
+  </div>
+  <div class="form-group">
+    <button
+    type="button"
+    class="btn btn-link"
+    @click="add()">
+      Add
+    </button>
+  </div>
+  <input type="submit">
+</form>
+</template>
+
+<script>
+export default {
+  name: 'create',
+  data(){
+    return {
+      title: "",
+      options: ["",""]
+    }
+  },
+  methods: {
+    onSubmit(){
+      console.log(this.title, this.options)
+    },
+    add(){
+      this.options.push("")
+    },
+    remove(index){
+      this.options.splice(index, 1)
+    }
+  }
+  
+}
+</script>
+
+<style scoped>
+</style>
